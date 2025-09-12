340cd49026f0fb632f1b649aa13643d1c6bca096
imlobster <zhizhilik@gmail.com>
2025-09-13 02:31:10 +0300

    done

---
+++ src/abit62.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstdint>
+#include <random>
+namespace abit62 {
+	inline uint64_t pcg=1;
+	inline bool init(const void* const ptr) { // pointer adress is pretty random
+		if(!ptr) return false;
+		pcg=reinterpret_cast<uintptr_t>(ptr);
+		return true;
+	}
+	inline bool string(char* const ic) {
+		if(!ic)return false;
+		uint8_t icend=0; // maximum length is 255 chars
+		for(;;++icend) {
+			if(ic[icend]=='\0')break;
+			if(icend>=255)return false;
+		}
+		for(uint8_t i=0;i<icend;++i) {
+			pcg=pcg*6364136223846793005ULL+1442695040888963407ULL; // PCG const's
+			uint8_t tc=(pcg>>32)%62;
+			ic[i]=tc<10?tc+48:(tc<36?tc+87:tc+29); // swapping 62 range with ascii codes
+		}
+		return true;
+	}
+}
